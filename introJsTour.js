84b7036475e70e0a5769d300bb84af27ff269507
dansvank <dansvank@gmail.com>
2023-02-10 12:39:47 -0500

    Testing

---
+++ introJsTour.js
@@ -0,0 +1,41 @@
+{
+	title: 'Main Menu',
+	intro: 'Just testing...'
+},
+{
+	element: document.querySelector('.Cashier'),
+	intro: 'This is Cashier'
+},
+{
+	element: document.querySelector('.Sports'),
+	intro: 'This is Sports'
+},
+{
+	element: document.querySelector('.Lotto'),
+	intro: 'This is Lotto'
+},
+{
+	element: document.querySelector('.Casino'),
+	intro: 'This is Casino'
+},
+{
+	element: document.querySelector('.Horses'),
+	intro: 'This is Horses'
+},
+{
+	title: 'For the Future',
+	intro: 'High hopes!'
+},
+{
+	element: document.querySelector('.Casino'),
+	intro: 'A good list of casinos'
+	openBefore: '.Casino'
+},
+{
+	element: document.querySelector('.Casino.open > div > a:nth-child(6)'),
+	intro: 'Should be Diamond Casino... or used to be.'
+},
+{
+	title: 'Have a good one',
+	intro: 'Cheers!'
+}
